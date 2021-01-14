8d774458ed9bff4492482e888fb9aec3ac850aa4
IckycHIk <32699764+IckycHIk@users.noreply.github.com>
2021-01-14 20:52:51 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# project_test_flutter
+ Test project for Solid Software
---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return  MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primaryColor: Colors.black,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+
+      ),
+      home: MyHomePage() ,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget{
+  
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+
+
+
+class _MyHomePageState extends State<MyHomePage> {
+  Color backgroundColor;
+
+  void changeColor(){
+    setState(() {
+      backgroundColor  = genRandomColor();
+    });
+  }
+
+  Color genRandomColor(){
+    var rng = new Random();
+    return Color.fromARGB(100, rng.nextInt(255), rng.nextInt(255), rng.nextInt(255));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return  GestureDetector(
+
+onTap: ()=>changeColor(),
+
+      child:Scaffold(
+    backgroundColor: backgroundColor ,
+      body:Center(
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Hey there', style: TextStyle(fontStyle: FontStyle.italic,height: 10, fontSize: 24),
+            ),
+          ],
+        ),
+        ),
+        ),
+    );
+  }
+
+
+}
+
+
+
